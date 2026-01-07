839b6c02fa9d40dbf0e0715a12a37e14738419b7
Ivan Alejandro Flores Macin <240793@utxicotepec.edu.mx>
2026-01-07 09:49:33 -0600

    Practica 02
    add:Se agrego el Archivo del Ejercicio 01 declaracion de Variables, Aun sin Terminar

---
+++ Js/Ejercicio01.js
@@ -0,0 +1,6 @@
+// 1. Declaracion de Variables utilizando el prefijo var
+var minombre = "Ivan";
+var miApellido = "Flores";
+var miEdad = 21;
+// 2. Imprimir en la consola
+console.log("Mi nombre es: " + minombre + " " + miApellido);
